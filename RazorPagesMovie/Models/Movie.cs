7a4d82f370f1d9a2705e6d3ddbe5480f8f50328a
Vitor Ferreira <victor-bash2@hotmail.com>
2022-09-16 14:26:01 -0300

    Added Movie data annotations, Movie menu, Movie create page

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -4,12 +4,19 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        [Key]
         public int ID { get; set; }
+
+        [DataType(DataType.Text)]
         public string Title { get; set; } = string.Empty;
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [DataType(DataType.Text)]
         public string Genre { get; set; } = string.Empty;
+
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
