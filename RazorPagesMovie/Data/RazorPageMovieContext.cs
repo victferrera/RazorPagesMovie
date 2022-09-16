da25890dc5e11f1e41b5a95f10f71a6f2ff82afc
Vitor Ferreira <victor-bash2@hotmail.com>
2022-09-16 14:52:22 -0300

    Added RazorPageMovieContext, modified Create page, added BindProperty on Movie, removed Nullable property in RazorPagesMovie.csproj

---
+++ RazorPagesMovie/Data/RazorPageMovieContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPageMovieContext : DbContext
+    {
+        public RazorPageMovieContext(DbContextOptions<RazorPageMovieContext> opt) : base(opt){}
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- RazorPagesMovie/Pages/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Create.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Movie Movie { get; set; }
         public void OnGet()
         {
         }
