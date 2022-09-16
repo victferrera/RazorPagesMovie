bc0d9f672edf9b564a849593c3ef80edf3c8d48d
Vitor Ferreira <victor-bash2@hotmail.com>
2022-09-16 16:38:17 -0300

    Added initial migration, changed layout,, added home page for create, reade, update and delete Movies

--- RazorPagesMovie/Pages/Create.cshtml.cs
+++ RazorPagesMovie/Pages/Create.cshtml.cs
@@ -1,15 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages
 {
     public class CreateModel : PageModel
     {
+        private readonly RazorPageMovieContext _context;
+
+        public CreateModel(RazorPageMovieContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public Movie Movie { get; set; }
-        public void OnGet()
+
+        public IActionResult OnGet()
         {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var movie = _context.Add(new Movie());
+
+            movie.CurrentValues.SetValues(Movie);
+            await _context.SaveChangesAsync();
+            return Redirect("Create");
         }
     }
 }
